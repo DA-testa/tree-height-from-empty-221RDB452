783d1a8e3877fa9d654430ee41272362054bf247
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-03-04 11:50:39 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,6 +1,7 @@
 # python3
 import sys
 import threading
+
 def compute_height(n, parents):
     tree={}
     for i in range(n):
@@ -18,14 +19,16 @@ def compute_height(n, parents):
         else:
             return max(height(child) for child in tree[node])+1
     return height(root)
+
 def main():
     try:
-        n=int(input())
+        n = int(input("Enter the number of nodes in the tree: "))
     except ValueError:
         print("Invalid input. Please enter an integer.")
         return
-    parents=list(map(int,input().split()))
+    parents = list(map(int,input("Enter the parent of each node separated by spaces: ").split()))
     print(compute_height(n,parents))
+
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
