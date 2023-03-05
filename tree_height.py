e6e2321c52e5ebd1a0ab36fc749cf21c2bb1a5f5
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-03-06 01:04:51 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,21 +6,23 @@ def compute_height(num_nodes, parents):
     parent=numpy.zeros(num_nodes)
     def height(i):
         if parent[i] !=0:
-            return parent [i]
+            return parent[i]
         if parents[i] == -1:
             parent[i] = 1
         else:
             parent[i]=height(parents[i]) +1
         return parent[i]
+    
     for i in range(num_nodes):
         height(i)
     return int(max(parent))
+
 def main():
     mode = input()
     if "F" in mode:
         filename = input()
-        if"a" not in filename:
-           with open(str("test/"+filename), mode ="r") as f:
+        if "a" not in filename:
+           with open(str("test/"+filename), mode = "r") as f:
                n = int(f.readline())
                parentOfNode = list(map(int, f.readline().split()))
         else:
