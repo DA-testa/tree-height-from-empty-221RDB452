a9634cbc0e502e6172ccd0a9ab67be71f13107a4
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-03-05 22:06:51 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,38 +1,40 @@
 import sys
-import threading
-def compute_height(n, parents):
+
+def construct_tree(n, parents):
     tree = {}
     for node, parent in enumerate(parents):
-        if parent ==-1:
+        if parent == -1:
             root = node
         else:
             if parent in tree:
                 tree[parent].append(node)
             else:
                 tree[parent] = [node]
+    return root, tree
+
+def compute_height(root, tree):
     def height(node):
         if node not in tree:
             return 0
-        return max(height(child) for child in tree[node])+1
+        return max(height(child) for child in tree[node]) + 1
     return height(root)
+
 def main():
     mode = input()
-    if "F" in mode:
-            filename = input()
-            if "a" not in filename:
-              with open(str("test/"+filename), mode = "r") as f:
-                n = int(f.readline())
-                parents = list(map(int, f.readline().split()))
-            else:
-                print("error")
-    elif "I" in mode:
-            n = int(input())
-            parents = list(map(int, input().split()))
+    if mode == 'F':
+        filename = input()
+        with open(filename, 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+    elif mode == 'I':
+        n = int(input())
+        parents = list(map(int, input().split()))
     else:
-            print("invalid mode.")
-    print(compute_height(n, parents))
-   
+        print("Invalid mode.")
+        return
+    root, tree = construct_tree(n, parents)
+    print(compute_height(root, tree))
+
 sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+main()
 
